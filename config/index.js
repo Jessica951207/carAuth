e0e37837f21b4a84b3d66085d630b14134920007
jessica <jessica.qy.li@cn.pwc.com>
2021-11-11 17:06:52 +0800

    change ip address

--- config/index.js
+++ config/index.js
@@ -34,8 +34,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '10.1.3.86', // can be overwritten by process.env.HOST
-    // host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '10.1.3.86', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
